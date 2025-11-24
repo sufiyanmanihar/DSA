9c31de6ea5cc579956e01e1e5396ab47d79459be
Sufiyan <sufiyanmanihar533@gmail.com>
2025-11-25 02:48:03 +0530

    Time: 0 ms (100%), Space: 65 MB (19.91%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currentSum = 0;
+    let maxSum = -Infinity;
+
+    for(let i=0;i<nums.length;i++){
+        currentSum += nums[i];
+        maxSum = Math.max(currentSum, maxSum);
+        if(currentSum<0)
+            currentSum = 0;
+    }
+    return maxSum;        
+};
