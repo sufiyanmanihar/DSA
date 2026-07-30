c1fb6a7411033c9a90dcd616011a072b9f928623
Sufiyan <sufiyanmanihar533@gmail.com>
2026-07-30 19:19:11 +0530

    Time: 4 ms (34.08%), Space: 19.4 MB (33.52%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.py
@@ -7,10 +7,11 @@ class Solution:
             if char.isdigit():
                 digit = int(char)
 
-                if digit>largest:
+                if digit > largest:
                     second = largest
                     largest = digit
 
-                elif largest>digit>second:
+                elif largest > digit > second:
                     second = digit
+
         return second
